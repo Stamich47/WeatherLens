47c8e5b33077e511d33d1f39187aebc373464fbe
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-08-06 16:26:47 -0700

    Update proxy server for CORS issue

--- src/components/TemperatureFetch.js
+++ src/components/TemperatureFetch.js
@@ -13,9 +13,10 @@ const WeatherComponent = () => {
       const username = process.env.REACT_APP_USER;
       const password = process.env.REACT_APP_PW;
       const endpoint = `https://api.meteomatics.com/${timeString}/t_2m:F/34.0536909,-118.242766/json`; // Los Angeles coordinates
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/"; // Public proxy server
       console.log(endpoint);
 
-      fetch(endpoint, {
+      fetch(proxyUrl + endpoint, {
         headers: {
           Authorization: "Basic " + btoa(`${username}:${password}`),
         },
