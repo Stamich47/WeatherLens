d3c8014958b27e2030296b525a59c6cb9497f59a
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-10-16 12:04:58 -0700

    Change API url for TemperatureFetch

--- src/components/TemperatureFetch.js
+++ src/components/TemperatureFetch.js
@@ -7,14 +7,15 @@ const WeatherComponent = () => {
 
   useEffect(() => {
     const fetchTemperature = () => {
-      const currentDate = new Date();
-      const timeString = currentDate.toISOString();
+      // const currentDate = new Date();
+      // const timeString = currentDate.toISOString();
 
       const username = process.env.REACT_APP_USER;
       const password = process.env.REACT_APP_PW;
-      const endpoint = `/api/meteomatics/${timeString}/t_2m:F/34.0536909,-118.242766/json`; // Los Angeles coordinates
+      const endpoint = `/api/v1/forecast?latitude=34.0522&longitude=-118.2437&current=temperature_2m&temperature_unit=fahrenheit&timezone=America%2FLos_Angeles&forecast_days=1`; // Los Angeles coordinates
 
       fetch(endpoint, {
+        mode: "cors",
         headers: {
           Authorization: "Basic " + btoa(`${username}:${password}`),
         },
@@ -23,17 +24,12 @@ const WeatherComponent = () => {
           if (!response.ok && response.status !== 403) {
             throw new Error(`HTTP error! Status: ${response.status}`);
           } else if (response.status === 403) {
-            throw (
-              new Error(`HTTP error! Status: ${response.status}`) &&
-              alert(
-                `403 Error! The weather API access is currently being run through a proxy server.\n\nOpen the Nav Dropdown and please select '403 Error - Get Access' to deploy the proxy server.`
-              )
-            );
+            throw new Error(`HTTP error! Status: ${response.status}`);
           }
           return response.json();
         })
         .then((data) => {
-          const tempValue = data.data[0].coordinates[0].dates[0].value;
+          const tempValue = data.current.temperature_2m;
           setTemperature(tempValue);
           setLoading(false);
         })
