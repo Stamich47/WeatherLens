62e8684971a0bf2322bdf2ae83f00a7adff0ef35
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-10-16 11:02:15 -0700

    Attempt to proxy through netlify.toml

--- src/components/TemperatureFetch.js
+++ src/components/TemperatureFetch.js
@@ -12,17 +12,17 @@ const WeatherComponent = () => {
 
       const username = process.env.REACT_APP_USER;
       const password = process.env.REACT_APP_PW;
-      const endpoint = `https://api.meteomatics.com/${timeString}/t_2m:F/34.0536909,-118.242766/json`; // Los Angeles coordinates
-      const proxyUrl = "https://cors.bridged.cc/";
+      const endpoint = `/api/${timeString}/t_2m:F/34.0536909,-118.242766/json`; // Los Angeles coordinates
 
-      fetch(proxyUrl + endpoint, {
+      fetch(endpoint, {
+        mode: "cors",
         headers: {
           Authorization: "Basic " + btoa(`${username}:${password}`),
         },
       })
         .then((response) => {
-          if (!response.ok && response.status !== 403) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status} `);
           }
           return response.json();
         })
